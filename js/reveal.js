b12e43673a0b3773ac78d4b0740c66d03e3692db
Paul Negedu <paul.negedu@yahoo.com>
2022-12-10 15:15:06 -0600

    bouncy-button + more changes

--- js/reveal.js
+++ js/reveal.js
@@ -1,16 +1,17 @@
 function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
-    for (var i = 0; i < reveals.length; i++) {
-        var windowHeight = window.innerHeight;
-        var elementTop = reveals[i].getBoundingClientRect().top;
-        var elementVisible = 150;
+    const reveals = document.querySelectorAll(".reveal");
+    for (const reveal of reveals) {
+        const windowHeight = window.innerHeight;
+        const elementTop = reveal.getBoundingClientRect().top;
+        const elementVisible = 150;
 
         if (elementTop < windowHeight - elementVisible) {
-            reveals[i].classList.add("active");
+            reveal.classList.add("active");
         } else {
-            reveals[i].classList.remove("active");
+            reveal.classList.remove("active");
         }
     }
 }
 
 window.addEventListener("scroll", reveal);
+
