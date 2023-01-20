718ee5350706d820260d6986d247746390f100a6
Paul Negedu <paul.negedu@yahoo.com>
2023-01-20 09:33:43 -0600

    hacky way to solve card issues onn iOS
    fix deskptop display+speed home page animations
    
    new pictures, fixed multiple reveal files

--- js/home.js
+++ js/home.js
@@ -1,4 +1,5 @@
-
+const banner_text = document.querySelector('#banner_text');
+const banner_head = document.querySelector('#banner_head');
 const mainHome = document.querySelector('#mainHome');
 const page = document.querySelector('#page');
 const cover = document.querySelector('#cover');
@@ -6,13 +7,11 @@ const landingBtn = document.querySelector("#landingBtn");
 landingBtn.addEventListener('click', () => {
     mainHome.classList.remove('hidden');
     page.classList.add('moved');
-    setTimeout(() =>
-        page.classList.remove('moved')
-        , 2100)
-    setTimeout(() =>
-        cover.classList.add('hidden')
-        , 2100);
-})
+    setTimeout(() => {
+        page.classList.remove('moved');
+        cover.classList.add('hidden');
+    }, 2100);
+});
 
 function reveal() {
     const reveals = document.querySelectorAll(".reveal");
@@ -32,4 +31,25 @@ function reveal() {
 window.addEventListener("scroll", reveal);
 setTimeout(() =>
     landingBtn.classList.remove('hidden-slow')
-    , 6200);
+    , 14100);
+
+setTimeout(() =>{
+    banner_head.classList.add('text_shadow');
+    banner_head.classList.add('bold');
+}, 7000);
+setTimeout(() =>
+    banner_head.classList.remove('hidden-vis')
+, 4999);
+setTimeout(() =>
+    banner_text.classList.add('text_shadow_small')
+, 6999);
+
+setTimeout(() =>
+banner_text.children[0].classList.remove('hidden-vis')
+, 8100);
+setTimeout(() =>
+banner_text.children[1].classList.remove('hidden-vis')
+, 10100);
+setTimeout(() =>
+banner_text.children[3].classList.remove('hidden-vis')
+, 12100);
