50ce223aa8636e02ff9f1ac1b3d6819715f54283
Paul Negedu <37233089+paulnegz@users.noreply.github.com>
2023-01-21 08:14:13 -0600

    hacky way to solve card issues onn iOS (#10)
    fix deskptop display+speed home page animations
    
    new pictures, fixed multiple reveal files
    
    made site secure. Redirect http to https

--- js/reveal.js
+++ js/reveal.js
@@ -1,6 +1,20 @@
-function reveal() {
-    const reveals = document.querySelectorAll(".reveal");
-    for (const reveal of reveals) {
+const reveals = document.querySelectorAll(".reveal");
+const revealListener = () =>{
+    revealFactory(reveals);
+}
+
+const sideveals = document.querySelectorAll(".sideveal");
+const siderevealListener= () =>{
+    revealFactory(sideveals);
+}
+
+const leftveals = document.querySelectorAll(".leftveal");
+const leftrevealListener= () =>{
+    revealFactory(leftveals);
+}
+
+const revealFactory = (revealInterface)=> {
+    for (const reveal of revealInterface) {
         const windowHeight = window.innerHeight;
         const elementTop = reveal.getBoundingClientRect().top;
         const elementVisible = 150;
@@ -12,6 +26,6 @@ function reveal() {
         }
     }
 }
-
-window.addEventListener("scroll", reveal);
-
+window.addEventListener("scroll", revealListener);
+window.addEventListener("scroll", siderevealListener);
+window.addEventListener("scroll", leftrevealListener);
